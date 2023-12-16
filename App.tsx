fc4b18b6658f2adf8661b5d7b391a6d29365208d
manvigautam09 <mnvsgautam9@gmail.com>
2023-12-16 20:58:05 +0530

    Adds home screen

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import Login from './src/screens/Login';
 import SignUp from './src/screens/SignUp';
+import Home from './src/screens/Home';
 
 function App(): React.JSX.Element {
   const Stack = createNativeStackNavigator();
@@ -15,6 +16,7 @@ function App(): React.JSX.Element {
         screenOptions={{headerShown: false}}>
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="SignUp" component={SignUp} />
+        <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
   );
