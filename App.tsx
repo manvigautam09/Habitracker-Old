42cfefedacb1d304f05f97aedfc34c58854a7eba
manvigautam09 <mnvsgautam9@gmail.com>
2023-12-18 00:23:31 +0530

    Added List

--- App.tsx
+++ App.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
+import Home from './src/screens/Home';
 import Login from './src/screens/Login';
 import SignUp from './src/screens/SignUp';
-import Home from './src/screens/Home';
 
 function App(): React.JSX.Element {
   const Stack = createNativeStackNavigator();
+  const queryClient = new QueryClient();
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Home"
-        screenOptions={{headerShown: false}}>
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="SignUp" component={SignUp} />
-        <Stack.Screen name="Home" component={Home} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Home"
+          screenOptions={{headerShown: false}}>
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="SignUp" component={SignUp} />
+          <Stack.Screen name="Home" component={Home} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </QueryClientProvider>
   );
 }
 
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,42 @@
+import {useQuery} from '@tanstack/react-query';
 import React from 'react';
-import {View, SafeAreaView, StyleSheet, Text} from 'react-native';
+import {View, SafeAreaView, StyleSheet, Text, FlatList} from 'react-native';
 
 function Home(): React.JSX.Element {
+  const {data} = useQuery({
+    queryKey: ['todos-list'],
+    queryFn: async () => {
+      const response = await fetch(
+        'https://jsonplaceholder.typicode.com/todos',
+      );
+      return response.json();
+    },
+  });
+
+  const renderSeparator = () => {
+    return <View style={styles.sepView} />;
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <View>
-        <Text>This is home screen</Text>
+        <Text>Showing List of Todos below</Text>
       </View>
+      {data && data.length > 0 ? (
+        <FlatList
+          data={data}
+          renderItem={({item}) => (
+            <View style={styles.todoItem}>
+              <Text style={styles.todoItemText}>{item.title}</Text>
+            </View>
+          )}
+          ItemSeparatorComponent={renderSeparator}
+        />
+      ) : (
+        <View>
+          <Text>Loading data...</Text>
+        </View>
+      )}
     </SafeAreaView>
   );
 }
@@ -18,5 +48,16 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     backgroundColor: '#c8b6ff',
+  },
+  sepView: {
+    height: 1,
+    width: '100%',
+    backgroundColor: '#fff',
+  },
+  todoItem: {
+    padding: 12,
+  },
+  todoItemText: {
+    textTransform: 'capitalize',
   },
 });
